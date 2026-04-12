efa68bc5429a474278cd0f81e6a7c2c059ffb32d
Pavel Salnikov <pavel.e.salnikov@gmail.com>
2026-04-12 07:37:11 -0700

    wip. Improve test command

--- test_command/test_cron.sh
+++ test_command/test_cron.sh
@@ -6,11 +6,11 @@ TIMEOUT="$1"; shift
 
 main(){
   local exit_code="$1"; shift
-  echo "STDOUT before the workload" >&1
-  echo "STDERR before the workload" >&2
+  echo "StdOut before the workload" >&1
+  echo "StdErr before the workload" >&2
   sleep "$TIMEOUT"
-  echo "STDOUT after the workload" >&1
-  echo "STDERR after the workload" >&2
+  echo "StdOut after the workload" >&1
+  echo "StdErr after the workload" >&2
   echo "Exiting. Exit code $exit_code" >&2
   exit "$exit_code"
 }
